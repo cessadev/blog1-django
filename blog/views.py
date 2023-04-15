b5dbcabaa6d6937e427fd2e6fe2b3731fef00616
César Urbiña <122382092+cessadev@users.noreply.github.com>
2023-04-15 11:08:23 -0500

    Add Blog Edit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import View, UpdateView
 
 from .forms import PostCreateForm
 from .models import Post
@@ -43,3 +44,13 @@ class BlogDetailView(View):
             'post':post
         }
         return render(request, 'blog_detail.html', context)
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
